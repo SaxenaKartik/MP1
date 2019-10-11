5198614f451cd818517ad04155b592f271dbb863
SaxenaKartik <kartiksaxena500@gmail.com>
2019-10-12 03:57:02 +0530

    Updated the RM and EDF scheduler

--- realtime_scheduler/earliest_deadline_first.py
+++ realtime_scheduler/earliest_deadline_first.py
@@ -8,52 +8,56 @@ class Process:
 		self.arrival_time = arrival_time
 		self.deadline = deadline
 
+
 class Scheduler:
-	def Schedule(self,list_process):
+	def schedule(self,list_process):
 		pass
 
 class EDF_Scheduler(Scheduler):
-	def schedule(self,list_process, total_slots):
+	def schedule(self,list_process, available_slots):
 		list_process.sort(key = lambda x: x.period)
 		list_process.sort(key = lambda x: x.deadline)
 
-		week_schedule = []
+		# for x in list_process:
+			# print(x.id, x.capacity, x.period)
+		# week_schedule = []
 		sum_slots = 0
-		for available_slots in total_slots:
-			for i in available_slots:
-				sum_slots+=i
+		# for available_slots in total_slots:
+		for i in available_slots:
+			sum_slots+=i
 
-			day_schedule = []
+		schedule = []
 
-			for p in list_process:
-				capacity = p.capacity
-				process_id = p.process_id
-				if sum_slots>0 and sum_slots>=capacity:
-					sum_slots-=capacity
-					while capacity>0:
-						day_schedule.append(process_id)
-						capacity-=0.5
-					# capacity = 0
+		for p in list_process:
+			capacity = p.capacity
+			process_id = p.process_id
+			if sum_slots>0 and sum_slots>=capacity:
+				sum_slots-=capacity
+				while capacity>0:
+					schedule.append(process_id)
+					capacity-=1
+				# capacity = 0
 
 
-				elif sum_slots>0 and sum_slots<capacity:
-					capacity -= sum_slots
-					while sum_slots>0:
-						day_schedule.append(process_id)
-						sum_slots -= 0.5
+			elif sum_slots>0 and sum_slots<capacity:
+				capacity -= sum_slots
+				while sum_slots>0:
+					schedule.append(process_id)
+					sum_slots -= 1
 
-				if sum_slots==0:
-					break
+			if sum_slots==0:
+				break
 
-			week_schedule.append(day_schedule)
-		return week_schedule
+		# week_schedule.append(schedule)
+		return schedule
+		# return week_schedule
 
 obj = EDF_Scheduler()
 process1 = Process(1,2, deadline = 5)
 process2 = Process(2,2, deadline = 20)
 process3 = Process(3,3, deadline = 10)
-slots = [[1,1,2,1,3],[.5, 1, 2.5, 1.5, 2], [1.5,1.5,1.5,1.5], [2,3,1], [1,1], [6], [7]]
+slots = [1,1,2,1,2]
 list_process = [process1, process2, process3]
-week_schedule = obj.schedule(list_process, slots)
-print(week_schedule)
+schedule = obj.schedule(list_process, slots)
+print(schedule)
 
