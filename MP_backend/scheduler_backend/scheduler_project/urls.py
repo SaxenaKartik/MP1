4ddea1fdd783cf5664a8ac53d259ac849d8c1ea7
SaxenaKartik <kartiksaxena500@gmail.com>
2019-11-11 22:54:37 +0530

    Updated the backend

--- MP_backend/scheduler_backend/scheduler_project/urls.py
+++ MP_backend/scheduler_backend/scheduler_project/urls.py
@@ -4,5 +4,21 @@ from . import views as views1
 from . import models 
 
 urlpatterns = [
-    path('', views1.layout)
+    path('', views1.home),
+    path('user/', views1.user),
+    # path('user/login', views.user_login),
+    # path('user/logout', views.user_logout),
+    # path('user/signup', views.user_signup),
+    path('user/<int:user_id>', views1.user_userid),
+    path('process', views1.process),
+    path('process/<int:process_id>', views1.process_processid),
+    path('schedule/', views1.schedule),
+    path('schedule/<int:user_id>', views1.schedule_user),
+    path('schedule/<int:user_id>/<int:week>', views1.schedule_user_week),
+    path('schedule/<int:user_id>/<int:week>/<int:day>', views1.schedule_user_week_day),
+    path('schedule/efficiency/<int:user_id>', views1.schedule_efficiency_user),
+    path('schedule/efficiency/<int:user_id>/<int:week>', views1.schedule_efficiency_user_week),
+    path('schedule/efficiency/<int:user_id>/<int:week>/<int:day>', views1.schedule_efficiency_user_week_day),
+    path('schedule/list_process/<int:user_id>/<int:week>', views1.schedule_listprocess_user_week),
+    path('schedule/parameters/<int:user_id>/<int:week>', views1.schedule_parameters_user_week)
 ]
--- MP_backend/scheduler_backend/scheduler_project/views.py
+++ MP_backend/scheduler_backend/scheduler_project/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+	return HttpResponse('This is home')
+
+def user(request):
+	return HttpResponse('these are all the users')
+
+def user_userid(request, user_id):
+	return HttpResponse('this returns details of a user')
+
+def process(request):
+	return HttpResponse('this is the list of all process')
+
+def process_processid(request, process_id):
+	return HttpResponse('this is the details of a single process')
+
+def schedule(request):
+	return HttpResponse('this is the list of all schedules')
+
+def schedule_user(request, user_id):
+	return HttpResponse('this is the list of all schedules of user')
+
+def schedule_user_week(request, user_id, week):
+	return HttpResponse('this is the list of all schedules of user of a week')
+
+def schedule_user_week_day(request, user_id, week, day):
+	return HttpResponse('this is the list of all schedules of user of a day')
+
+def schedule_efficiency_user(request, user_id):
+	return HttpResponse('this returns the efficiency of a user for all weeks')
+
+def schedule_efficiency_user_week(request, user_id, week):
+	return HttpResponse('this returns the efficiency of a user for a weeks')
+
+
+def schedule_efficiency_user_week_day(request, user_id, week, day):
+	return HttpResponse('this returns the efficiency of a user for a day')	
+
+
+def schedule_listprocess_user_week(request, user_id, week):
+	return HttpResponse('this returns the list of process of a user for a weeks')
+
+
+def schedule_parameters_user_week(request, user_id, week):
+	return HttpResponse('this returns the parameters of a user for a weeks')
