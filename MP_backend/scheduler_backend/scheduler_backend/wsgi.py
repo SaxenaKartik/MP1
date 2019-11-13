cfd8c8a29e3a5d2719e8d32647f363ff09ff471d
SaxenaKartik <kartiksaxena500@gmail.com>
2019-11-13 17:32:24 +0530

    Updated the wsgi file

--- MP_backend/scheduler_backend/scheduler_backend/wsgi.py
+++ MP_backend/scheduler_backend/scheduler_backend/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scheduler_backend.settings')
 
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'scheduler_backend.settings'
+
 application = get_wsgi_application()
